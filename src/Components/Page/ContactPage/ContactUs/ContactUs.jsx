b8a06fdda6decc3db67c01005a9d29ccfc376adb
ibrohim74 <xibroxim@gmail.com>
2024-12-10 02:00:58 +0500

    com

--- src/Components/Page/ContactPage/ContactUs/ContactUs.jsx
+++ src/Components/Page/ContactPage/ContactUs/ContactUs.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './ContactUs.css'
 import foto from '../../../../Assisstest/img/contact.jpg'
+import EastIcon from '@mui/icons-material/East';
 export default function ContactUs() {
     return (
         <div className='contactus'>
@@ -14,19 +15,19 @@ export default function ContactUs() {
                     </h1>
                     <form action="">
                         <input type="text" placeholder='Имя' />
-                        <input type="email" placeholder='Mail' />
+                        <input type="email" placeholder='Почта' />
                         <input type="text" placeholder='Телефон' />
-                        <textarea name="" id="" cols="" rows="5" placeholder='Почта'></textarea>
-                        <a className='contact' href="https://maps.app.goo.gl/i6uGKFHJbpeTyzXPA">Submit</a>
+                        <textarea name="" id="" cols="" rows="5" placeholder='Сообщение'></textarea>
+                        <a className='contact' href={"#"}>Отправить</a>
                     </form>
-                    <h1 className="top">
-                    Наши координаты
-                    </h1>
+                    <a href={"https://maps.app.goo.gl/i6uGKFHJbpeTyzXPA"} className="top">
+                    Наши координаты <EastIcon/>
+                    </a>
                     <p className="top">Электронная почта</p>
-                    <a href="#!" className="email">atlestextile@gmail.com</a>
+                    <a href="mailto:atlestextile@gmail.com" className="email">atlestextile@gmail.com</a>
                     <p className="title">Свяжитесь с нами</p>
                     <a href="tel:+998941582000" className="number">+998(94)158-20-00</a>
-                    <p className="bottom">ул. Афросиаб,16 100031, <br/>г.Ташкент Узбекистан</p>
+                    <a href={"https://maps.app.goo.gl/i6uGKFHJbpeTyzXPA"} className="bottom">ул. Афросиаб,16 100031, <br/>г.Ташкент Узбекистан</a>
                     <p className="media">Мы в социальных сетях</p>
                     <div className="icon">
                         <div className="i"><i class="fa-brands fa-twitter"></i></div>
