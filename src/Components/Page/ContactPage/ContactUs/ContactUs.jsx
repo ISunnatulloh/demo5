050b916f8be64ce31a5615d98c08356db4541515
“ISunnatulloh” <sunnatullohibragimov@gmail.com>
2024-12-11 15:48:44 +0500

    tugatldi

--- src/Components/Page/ContactPage/ContactUs/ContactUs.jsx
+++ src/Components/Page/ContactPage/ContactUs/ContactUs.jsx
@@ -8,32 +8,33 @@ export default function ContactUs() {
         <div className='contactus'>
             <div className="box">
                 <div className="one">
-                    <img src={foto} alt=""/>
+                    <img src={foto} alt="" />
                 </div>
                 <div className="two">
                     <h1 className="top">
-                        Мы всегда рады <br/>вашим вопросам!
+                        Мы всегда рады <br />вашим вопросам!
                     </h1>
                     <form action="">
-                        <input type="text" placeholder='Имя'/>
-                        <input type="email" placeholder='Почта'/>
-                        <input type="text" placeholder='Телефон'/>
+                        <input type="text" placeholder='Имя' />
+                        <input type="email" placeholder='Почта' />
+                        <input type="text" placeholder='Телефон' />
                         <textarea name="" id="" cols="" rows="5" placeholder='Сообщение'></textarea>
                         <a className='contact' href={"#"}>Отправить</a>
                     </form>
                     <a href={"https://maps.app.goo.gl/i6uGKFHJbpeTyzXPA"} className="top">
-                        Наши координаты <EastIcon/>
+                        Наши координаты <EastIcon />
                     </a>
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d2886.323785509983!2d71.055892!3d40.895705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDDCsDUzJzQ0LjUiTiA3McKwMDMnMjEuMiJF!5e1!3m2!1sru!2s!4v1733810183129!5m2!1sru!2s"
                         width="600" height="250" allowFullScreen="" loading="lazy"
-                        referrerPolicy="no-referrer-when-downgrade"></iframe>
+                        referrerPolicy="no-referrer-when-downgrade">
+                    </iframe>
                     <p className="top">Электронная почта</p>
                     <a href="mailto:atlestextile@gmail.com" className="email">atlestextile@gmail.com</a>
                     <p className="title">Свяжитесь с нами</p>
                     <a href="tel:+998941582000" className="number">+998(94)158-20-00</a>
                     <a href={"https://maps.app.goo.gl/i6uGKFHJbpeTyzXPA"} className="bottom">ул. Афросиаб,16
-                        100031, <br/>г.Ташкент Узбекистан</a>
+                        100031, <br />г.Ташкент Узбекистан</a>
                     <p className="media">Мы в социальных сетях</p>
                     <div className="icon">
                         <div className="i"><i className="fa-brands fa-facebook-f"></i></div>
