48518181f75aa3ee4473a92a7371e731c54dcfd4
ibrohim74 <xibroxim@gmail.com>
2024-12-20 22:52:50 +0500

    com

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -5,68 +5,73 @@ import foto1 from '../../Assisstest/img/foto1.jpg'
 import foto2 from '../../Assisstest/img/foto2.jpg'
 import foto3 from '../../Assisstest/img/foto3.jpg'
 import foto4 from '../../Assisstest/img/foto4.jpg'
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
+
 export default function Services() {
-  return (
-    <section className="services">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <p className="top">//От идеи до готового изделия</p>
-            <h1 className="top">Открой для себя, как текстиль может изменить стиль!</h1>
-          </div>
-        </div>
-      </div>
-      <div className="title">
-        <Link to='/fabric' className="box">
-          <div className="foto">
-            <img src={foto1} alt="" />
-          </div>
-          <p className="one">01 - Швейный<br />цех</p>
-          <div className="bottom">
-            <p className="two">
-              Передовое оборудование и <br />инновационные методы производства.
-            </p>
-            <NorthEastIcon />
-          </div>
-        </Link>
-        <Link to='/fabric' className="box">
-          <div className="foto">
-            <img src={foto2} alt="" />
-          </div>
-          <p className="one">02 - Раскройный<br />цех</p>
-          <div className="bottom">
-            <p className="two">
-              От повседневной одежды <br />до эксклюзивных коллекций.
-            </p>
-            <NorthEastIcon />
-          </div>
-        </Link>
-        <Link to='/fabric' className="box">
-          <div className="foto">
-            <img src={foto3} alt="" />
-          </div>
-          <p className="one">03 - Гладильно<br />упаковочный цех</p>
-          <div className="bottom">
-            <p className="two">
-              Только лучшие материалы и<br /> тщательный контроль на каждом этапе.
-            </p>
-            <NorthEastIcon />
-          </div>
-        </Link>
-        <Link to='/fabric' className="box">
-          <div className="foto">
-            <img src={foto4} alt="" />
-          </div>
-          <p className="one">04 - Контроль<br />качества</p>
-          <div className="bottom">
-            <p className="two">
-              Учёт потребностей каждого клиента<br /> и персонализированные решения для <br />создания уникальной продукции.
-            </p>
-            <NorthEastIcon />
-          </div>
-        </Link>
-      </div>
-    </section>
-  )
+    return (
+        <section className="services">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <p className="top">//От идеи до готового изделия</p>
+                        <h1 className="top">Открой для себя, как текстиль может изменить стиль!</h1>
+                    </div>
+                </div>
+            </div>
+            <div className="title">
+                <Link to='/fabric' className="box">
+                    <div className="foto">
+                        <img src={foto1} alt=""/>
+                    </div>
+                    <p className="one">01 - Швейный<br/>цех</p>
+                    <div className="bottom">
+                        <p className="two">
+                            Оснащён современным оборудованием <br/> и высококвалифицированным персоналом.
+                        </p>
+                        <NorthEastIcon/>
+                    </div>
+                </Link>
+                <Link to='/fabric' className="box">
+                    <div className="foto">
+                        <img src={foto2} alt=""/>
+                    </div>
+                    <p className="one">02 - Раскройный<br/>цех</p>
+                    <div className="bottom">
+                        <p className="two">
+                            Оснащён передовыми технологиями <br/> и высокоточными машинами,<br/> обеспечивающими
+                            точность и
+                            скорость раскроя <br/> тканей для дальнейшего производства
+                        </p>
+                        <NorthEastIcon/>
+                    </div>
+                </Link>
+                <Link to='/fabric' className="box">
+                    <div className="foto">
+                        <img src={foto3} alt=""/>
+                    </div>
+                    <p className="one">03 - Гладильно<br/>упаковочный цех</p>
+                    <div className="bottom">
+                        <p className="two">
+                            Оснащён современным оборудованием для <br/> качественной глажки и аккуратной <br/> упаковки
+                            готовых изделий
+                        </p>
+                        <NorthEastIcon/>
+                    </div>
+                </Link>
+                <Link to='/fabric' className="box">
+                    <div className="foto">
+                        <img src={foto4} alt=""/>
+                    </div>
+                    <p className="one">04 - Контроль<br/>качества</p>
+                    <div className="bottom">
+                        <p className="two">
+                            Включает многократные проверки на каждом <br/> этапе производства, гарантируя высокие стандарты
+                            <br/> продукции и соответствие международным требованиям
+                        </p>
+                        <NorthEastIcon/>
+                    </div>
+                </Link>
+            </div>
+        </section>
+    )
 }
--- src/Components/Why/Why.jsx
+++ src/Components/Why/Why.jsx
@@ -2,66 +2,6 @@ import React, { useEffect, useState, useRef } from 'react'
 import './Why.css'
 import star from '../../Assisstest/img/star.png'
 export default function Why() {
-    // const [count1, setCount1] = useState(0);
-    // const [count2, setCount2] = useState(0);
-    // const [count3, setCount3] = useState(0);
-
-    // // Create ref to the section you want to track
-    // const sectionRef = useRef(null);
-
-    // // 1st number counting animation
-    // useEffect(() => {
-    //    const tim1 = setInterval(() => {
-    //         const timer1 = setInterval(() => {
-    //             setCount1((prevCount) => {
-    //                 if (prevCount < 70) {
-    //                     return prevCount + 1;
-    //                 } else {
-    //                     clearInterval(timer1);
-    //                     return prevCount;
-    //                 }
-    //             });
-    //         }, 140);
-    //         return () => clearInterval(timer1);
-    //     }, 7000);
-    //     return () => clearInterval(tim1)
-    // }, []);
-
-    // // 2nd number counting animation
-    // useEffect(() => {
-    //     const tim2 = setInterval(() => {
-    //         const timer2 = setInterval(() => {
-    //             setCount2((prevCount) => {
-    //                 if (prevCount < 96) {
-    //                     return prevCount + 1;
-    //                 } else {
-    //                     clearInterval(timer2);
-    //                     return prevCount;
-    //                 }
-    //             });
-    //         }, 100);
-    //         return () => clearInterval(timer2);
-    //     }, 7000);
-    //     return () => clearInterval(tim2)
-    // }, []);
-
-    // // 3rd number counting animation
-    // useEffect(() => {
-    //     const tim3 = setInterval(() => {
-    //         const timer3 = setInterval(() => {
-    //             setCount3((prevCount) => {
-    //                 if (prevCount < 92) {
-    //                     return prevCount + 1;
-    //                 } else {
-    //                     clearInterval(timer3);
-    //                     return prevCount;
-    //                 }
-    //             });
-    //         }, 100);
-    //         return () => clearInterval(timer3);
-    //     }, 7000);
-    //     return () => clearInterval(tim3)
-    // }, []);
     return (
         <section className='why'>
             <div className="container">
@@ -95,7 +35,6 @@ export default function Why() {
                         </div>
                     </div>
                 </div>
-
             </div>
             <div className="carousel">
                 <div className="logo-slider">
